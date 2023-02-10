f9711bc124ad8d1ffab48b01c871b8b64f965847
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-10 12:40:41 +0100

    initialized repository

---
+++ main.js
@@ -0,0 +1,74 @@
+let tasks = [{
+        "taskName" : "Shopping", 
+        "image" : "", 
+        "description" : "Get basic groceries from supermarket", 
+        "priority" : 0, 
+        "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}, {
+    "taskName" : "Shopping", 
+    "image" : "", 
+    "description" : "Get basic groceries from supermarket", 
+    "priority" : 0, 
+    "periodical" : "3x a week"  
+}
+]
