2d2fa6e07d1afc5ae7c36bbef74952122c643530
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-10 16:09:00 +0100

    filled in content

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ for(let val of tasks){
     document.getElementById("task-cards").innerHTML += `
 <div class="col">
     <div class="card shadow" style="width: 18rem;">
-        <img src="${val.image}" class="card-img-top" alt="${val.taskName}">
+        <img src="${val.image}" class="card-img-top imgSize" alt="${val.taskName}">
         <div class="card-body">
         <h5 class="card-title">${val.taskName}</h5>
         <hr>
@@ -11,7 +11,7 @@ for(let val of tasks){
         <p class="periodical">${val.periodical}</p>
         <hr>
         <p>Priority-Level: <span class="priority p-1 rounded">${val.priority}</span></p>
-        <p class="btn btn-primary PriorityBtn">Priority</p>
+        <p class="btn btn-dark PriorityBtn">Priority</p>
         </div>
     </div>
 </div>`;
--- taskdata.js
+++ taskdata.js
@@ -1,74 +1,74 @@
 let tasks = [{
     "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
+    "image" : "https://cdn.pixabay.com/photo/2015/08/07/16/07/shopping-879498_1280.jpg", 
+    "description" : "Get basic groceries from supermarket.", 
     "priority" : 0, 
     "periodical" : "3x a week"  
 }, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
+"taskName" : "Cooking", 
+"image" : "https://cdn.pixabay.com/photo/2016/11/19/11/38/avocado-1838785_1280.jpg", 
+"description" : "Make enough, so we can eat leftovers for lunch the next day.", 
+"priority" : 0, 
+"periodical" : "1x a day"  
+}, {
+"taskName" : "Walking the dog", 
+"image" : "https://cdn.pixabay.com/photo/2016/11/26/23/45/dog-1861839_1280.jpg", 
+"description" : "Two longer walks for trainig, two times just for relief.", 
+"priority" : 0, 
+"periodical" : "4x a day"  
+}, {
+"taskName" : "Laundry", 
+"image" : "https://cdn.pixabay.com/photo/2014/06/05/15/49/trousers-362781_1280.jpg", 
+"description" : "Washing dirty laundry and hanging up to dry.", 
+"priority" : 0, 
+"periodical" : "Up to 2 times a week"  
+}, {
+"taskName" : "Washing Dishes", 
+"image" : "https://cdn.pixabay.com/photo/2016/10/24/21/50/plate-1767422_1280.jpg", 
+"description" : "Use dishwasher for the most part, wash delicate dishes by hand", 
+"priority" : 0, 
+"periodical" : "1x a day"  
+}, {
+"taskName" : "Cleaning", 
+"image" : "https://cdn.pixabay.com/photo/2015/03/03/18/39/vacuum-cleaner-657719_1280.jpg", 
+"description" : "Vacuuming the apartment and cleaning the bathroom", 
+"priority" : 0, 
+"periodical" : "1x a week"  
+}, {
+"taskName" : "Cleaning windows", 
+"image" : "https://cdn.pixabay.com/photo/2016/06/16/13/00/rain-stoppers-1461288_1280.jpg", 
+"description" : "Cleaning the windows, make sure to check weather forecast", 
+"priority" : 0, 
+"periodical" : "2x a year"  
+}, {
+"taskName" : "Team-Meeting", 
+"image" : "https://cdn.pixabay.com/photo/2015/01/09/11/08/startup-594090_1280.jpg", 
+"description" : "Every Thursday at 16:00. Don't forget project report.", 
+"priority" : 0, 
+"periodical" : "1x a week"  
+}, {
+"taskName" : "Bring Car to Workshop", 
+"image" : "https://cdn.pixabay.com/photo/2019/11/20/19/15/jeep-4640909_1280.jpg", 
+"description" : "Motor makes funny noises. Has to get checked.", 
+"priority" : 0, 
+"periodical" : "not a regular task"  
+}, {
+"taskName" : "Go to Post-Office", 
+"image" : "https://cdn.pixabay.com/photo/2018/09/12/08/05/communication-3671504_1280.jpg", 
+"description" : "Send back Amazon-package, broken and get some stamps.", 
+"priority" : 0, 
+"periodical" : "not a regular task"  
+}, {
+"taskName" : "Sport", 
+"image" : "https://cdn.pixabay.com/photo/2016/03/08/22/14/sport-1244925__480.jpg", 
+"description" : "Do the fitness routine and go running", 
 "priority" : 0, 
 "periodical" : "3x a week"  
 }, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
+"taskName" : "Code Review", 
+"image" : "https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_1280.jpg", 
+"description" : "Finish Code Review. Make it as good as possible", 
 "priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
-}, {
-"taskName" : "Shopping", 
-"image" : "", 
-"description" : "Get basic groceries from supermarket", 
-"priority" : 0, 
-"periodical" : "3x a week"  
+"periodical" : "1x every 2 weeks"  
 }
 ]
