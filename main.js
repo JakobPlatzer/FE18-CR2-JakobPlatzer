736225375c133ef1b56975c572794dac44194dd7
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-10 15:19:17 +0100

    finished html-layout

--- main.js
+++ main.js
@@ -1,74 +1,33 @@
-let tasks = [{
-        "taskName" : "Shopping", 
-        "image" : "", 
-        "description" : "Get basic groceries from supermarket", 
-        "priority" : 0, 
-        "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
-}, {
-    "taskName" : "Shopping", 
-    "image" : "", 
-    "description" : "Get basic groceries from supermarket", 
-    "priority" : 0, 
-    "periodical" : "3x a week"  
+for(let val of tasks){
+    document.getElementById("task-cards").innerHTML += `
+<div class="col">
+    <div class="card shadow" style="width: 18rem;">
+        <img src="${val.image}" class="card-img-top" alt="${val.taskName}">
+        <div class="card-body">
+        <h5 class="card-title">${val.taskName}</h5>
+        <hr>
+        <p class="description">${val.description}</p>
+        <hr>
+        <p class="periodical">${val.periodical}</p>
+        <hr>
+        <p>Priority-Level: <span class="priority p-1 rounded">${val.priority}</span></p>
+        <p class="btn btn-primary PriorityBtn">Priority</p>
+        </div>
+    </div>
+</div>`;
 }
-]
+
+let buttons = document.getElementsByClassName("PriorityBtn");
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", function(){
+        tasks[i].priority++;
+        console.log(tasks[i].priority);
+        document.getElementsByClassName("priority")[i].innerHTML = tasks[i].priority;
+        document.getElementsByClassName("priority")[i].style.backgroundColor = "green";
+        document.getElementsByClassName("priority")[i].style.color = "white";
+    })
+}
+
+
+
